cfa6bb9c4cfb939121dc4cc13981453483b04c45
Kevin <peng.kevin@me.com>
2020-08-02 01:16:42 -0400

    Initial Commit

---
+++ shenji-ios/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  shenji-ios
+//
+//  Created by Kevin Peng on 2020-08-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
