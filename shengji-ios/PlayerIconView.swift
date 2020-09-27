6f74248f2e544f413615d2c8b332d7f349508a95
Kevin <peng.kevin@me.com>
2020-09-26 23:18:33 -0400

    [FEATURE] Add player icon view

--- shengji-ios/PlayerIconView.swift
+++ shengji-ios/PlayerIconView.swift
@@ -9,18 +9,59 @@ import SwiftUI
 
 protocol PlayerProtocol {
   var name: String { get }
-  var teammate: Int { get }
+  var team: Int { get }
   var id: Int { get }
 }
 
-struct PlayerIconView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct Player: PlayerProtocol {
+  let name: String
+  let team: Int
+  let id: Int
+}
+
+struct Frame: ViewModifier {
+  let color: Color
+  func body(content: Content) -> some View {
+    content
+      .padding()
+      .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+      .border(color, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+  }
+}
+
+struct TeamFrame: ViewModifier {
+  let team: Int
+  
+  func body(content: Content) -> some View {
+    content
+      .modifier(Frame(color: color(for: team)))
+  }
+  
+  private func color(for team: Int) -> Color {
+    switch team {
+    case 1:
+      return .red
+    case 2:
+      return .blue
+    default:
+      return .primary
     }
+  }
+}
+
+struct PlayerIconView: View {
+  let player: PlayerProtocol
+  
+  var body: some View {
+    Text(player.name)
+      .modifier(TeamFrame(team: player.team))
+  }
 }
 
 struct PlayerIconView_Previews: PreviewProvider {
-    static var previews: some View {
-        PlayerIconView()
-    }
+  static var previews: some View {
+    PlayerIconView(
+      player: Player(name: "AD", team: 2, id: 1)
+    )
+  }
 }
